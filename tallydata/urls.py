e8fb7bcc09d0cda073804eab7d5310a4dabc15ef
arpitamundra <160219003+arpitamundra@users.noreply.github.com>
2024-03-23 00:58:53 +0530

    Update urls.py

--- tallydata/urls.py
+++ tallydata/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tallydata/upload', views.upload_file, name='upload_file'),
+    path('', views.upload_file, name='upload_file'),
     path('success/', views.success_view, name='success'),
 ]
