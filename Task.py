1ccafe25f5314712f310f98490fd6deb04e74e37
arpitamundra <160219003+arpitamundra@users.noreply.github.com>
2024-03-23 00:19:57 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,36 +1,51 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 import os
-
+from datetime import datetime
 def parse_xml(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
     transactions = []
-    def find_element(parent, tag):
-        element = parent.find(f".//{tag}")
-        return element.text if element is not None else ''
 
     for voucher in root.findall('.//VOUCHER'):
         try:
-            voucher_type = find_element(voucher, 'VOUCHERTYPENAME')
+            voucher_type = voucher.find('.//VOUCHERTYPENAME').text
             if voucher_type == 'Receipt':
-                mix_date = find_element(voucher, 'DATE')
+                mix_date = voucher.find('DATE').text
+                # mix_date = find_element(voucher, 'DATE')
                 date = datetime.strptime(mix_date, "%Y%m%d").date()
-                transaction_type = find_element(voucher, 'BANKALLOCATIONS.LIST/TRANSFERMODE')
-                vch_no = find_element(voucher, 'VOUCHERNUMBER')
-                ref_no = find_element(voucher, 'BANKALLOCATIONS.LIST/UNIQUEREFERENCENUMBER')
-                ref_type = find_element(voucher, 'BILLALLOCATIONS.LIST/BILLTYPE')
-                re_date = find_element(voucher, 'BANKALLOCATIONS.LIST/DATE')
-                ref_date = datetime.strptime(re_date, "%Y%m%d").date()
-                debtor = find_element(voucher, 'PARTYLEDGERNAME')
-                ref_amount = find_element(voucher, 'BILLALLOCATIONS.LIST/AMOUNT')
-                amount = find_element(voucher, 'ALLLEDGERENTRIES.LIST/AMOUNT')
-                particulars = find_element(voucher, 'ALLLEDGERENTRIES.LIST/LEDGERNAME')
-                vch_type = voucher_type
-                amount_verified = find_element(voucher, 'ALLLEDGERENTRIES.LIST/AMOUNT')
+                vch_type = voucher.find('VOUCHERTYPENAME').text
+                vch_no = voucher.find('VOUCHERNUMBER').text
+                debtor = voucher.find('PARTYLEDGERNAME').text
+                transactions.append({'Date': date, 'transaction_type': "Parent", "Vch No.": vch_no, "Ref No.": "NA",
+                                     "Ref Type": "NA", "Ref Date": "NA", "Debtor": debtor, "Ref Amount": "NA",
+                                     'Amount': voucher.find('ALLLEDGERENTRIES.LIST/AMOUNT').text, "Particulars": debtor,
+                                     "Vch Type": vch_type,
+                                     "Amount Verified": "Yes"})
 
-                transactions.append({'Date': date, 'transaction_type': transaction_type,"Vch No.":vch_no,"Ref No.":ref_no,"Ref Type":ref_type,"Ref Date":ref_date,"Debtor":debtor,"Ref Amount":ref_amount, 'Amount': amount,"Particulars":particulars,"Vch Type":vch_type,"Amount Verified":amount_verified})
+                for ledger_entry in voucher.findall('.//ALLLEDGERENTRIES.LIST'):
+                    ledger_name = ledger_entry.find('LEDGERNAME').text
+                    amount = ledger_entry.find('AMOUNT').text
+                    if ledger_name == debtor:
+                        for allocation in ledger_entry.findall('BILLALLOCATIONS.LIST'):
+                            transactions.append({'Date': date, 'transaction_type': "Child", "Vch No.": vch_no,
+                                                 "Ref No.": allocation.find('NAME').text,
+                                                 "Ref Type": allocation.find('BILLTYPE').text,
+                                                 "Ref Date": " ", "Debtor": ledger_name,
+                                                 "Ref Amount": allocation.find('AMOUNT').text,
+                                                 'Amount': "NA", "Particulars": ledger_name, "Vch Type": vch_type,
+                                                 "Amount Verified": "NA"})
+                for ledger_entry in voucher.findall('.//ALLLEDGERENTRIES.LIST'):
+                    ledger_name = ledger_entry.find('LEDGERNAME').text
+                    amount = ledger_entry.find('AMOUNT').text
+                    if ledger_name != debtor:
+                        transactions.append({'Date': date, 'transaction_type': "Other", "Vch No.": vch_no,
+                                             "Ref No.": "NA",
+                                             "Ref Type": "NA",
+                                             "Ref Date": "NA", "Debtor": ledger_name, "Ref Amount": "NA",
+                                             'Amount': amount, "Particulars": ledger_name, "Vch Type": vch_type,
+                                             "Amount Verified": "NA"})
         except Exception as e:
             print(f"Error processing voucher: {e}")
 
